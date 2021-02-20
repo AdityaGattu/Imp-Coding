825cdbf8dbf356850e8c344dd697ecfd7b3d11d4
AdityaGattu <52188412+AdityaGattu@users.noreply.github.com>
2021-02-20 23:40:19 +0530

    Update 1220. Count Vowels Permutation.cpp

--- Paypal/1220. Count Vowels Permutation.cpp
+++ Paypal/1220. Count Vowels Permutation.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
 
         int countVowelPermutation(int n) {
-        
-            
-            
-	long long mod = 1000000007;
+      
+	long long mod = 1000000007; // if no>1e9
 
-	vector<long long>dp(6, 1);
-	vector<long long>curdp(6, 0);
+	vector<long long>dp(6, 1);        // intial dp for single string aphabelts
+	vector<long long>curdp(6, 0);     // for updation
 
 
-	if (n == 1) {return 5;}
+	if (n == 1) {return 5;} 
 
-	for (int i = 1; i < n ; i++)
+	for (int i = 1; i < n ; i++)                    //  1->a  2->b 3->c 4->d 5->e
 	{
-		curdp[1] = ((dp[2] + dp[3]) % mod + dp[5]) % mod;
-		curdp[2] = (dp[1] + dp[3]) % mod;
-		curdp[3] = ((dp[2] + dp[4]) % mod);
-		curdp[4] = (dp[3]) % mod;
-		curdp[5] = (dp[4] + dp[3]) % mod;
+		curdp[1] = ((dp[2] + dp[3]) % mod + dp[5]) % mod;           // append a only if previous values(last character) are e or i or o this give no of a's in nxt iterartion
+		curdp[2] = (dp[1] + dp[3]) % mod;                           // append e only if previous values are a or i
+  		curdp[3] = ((dp[2] + dp[4]) % mod);			   // append i only if previous values	are e or o
+		curdp[4] = (dp[3]) % mod;			           // append o only if previous values is i 
+ 		curdp[5] = (dp[4] + dp[3]) % mod;		           // append u only if previous values are i or o
 
 		for (int ii = 1; ii <= 5; ii++)
 		{
