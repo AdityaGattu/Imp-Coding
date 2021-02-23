32b2359d7fc87f13004145434c6dee08f1b5c978
AdityaGattu <52188412+AdityaGattu@users.noreply.github.com>
2021-02-23 22:28:22 +0530

    Paypal

---
+++ Paypal/Intersection of two arrays.cpp
@@ -0,0 +1,31 @@
+int NumberofElementsInIntersection (int a[], int b[], int n, int m )
+{
+    // Your code goes here
+    
+    
+    map<int,int>m1;
+    set<int>s;
+    
+    for(int i=0;i<n;i++)
+    {
+        m1[a[i]]++;
+    }
+    
+    int cnt=0;
+    
+    
+    for(int i=0;i<m;i++)
+    {
+        s.insert(b[i]);
+    }
+    
+    for(auto itr=s.begin();itr!=s.end();itr++)
+    {
+        
+        if(m1[*itr]>=1)
+        cnt++;
+    }
+    
+    
+    return cnt;
+}
